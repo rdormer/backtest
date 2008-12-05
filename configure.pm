0afb1a4ef503ec4bcbf96a58564560347b98f95b
Robert Dormer <rdormer@gmail.com>
2008-12-05 18:18:59 +0000

    Add using this name instead.

---
+++ configure.pm
@@ -0,0 +1,24 @@
+package configure;
+require Exporter;
+
+use vars qw/@ISA @EXPORT $VERSION/;
+@EXPORT = qw/process_commandline/;
+@ISA = qw/Exporter/;
+$VERSION = 1.0;
+
+my $ticker_list;
+my $entry_file;
+my $exit_file;
+
+sub process_commandline {
+
+    while($_ = shift @_) {
+
+
+    }
+}
+
+
+
+
+1;
