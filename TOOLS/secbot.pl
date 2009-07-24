a16dd631c873163bb08d44b3477146eaa3aadf9d
Robert Dormer <rdormer@gmail.com>
2009-07-24 00:58:59 +0000

    Command line args, arg to set a data directory, and check for existance of downloaded reports before downloading.

--- TOOLS/secbot.pl
+++ TOOLS/secbot.pl
@@ -1,6 +1,10 @@
 #! /usr/bin/perl
 
+use Getopt::Long;
 use Net::FTP;
+
+my $dataroot;
+GetOptions('dataroot=s' => \$dataroot);
 
 print "\nConnecting to SEC server...";
 $ftpbot = Net::FTP->new("ftp.sec.gov") or die "CONNECT FAIL";
@@ -10,26 +14,33 @@ print "OK";
 
 #first step is to retrieve the index file
 
+if($dataroot ne "") {
+    chdir $dataroot;
+}
+
 print "\nRetrieve Index...";
 $ftpbot->binary();
 $ftpbot->cwd("/edgar/full-index") or die "CWD FAIL";
 $ftpbot->get("master.gz") or die "GET FAIL";
 print "OK"; 
 
-`gunzip master.gz`;
+`gunzip --force master.gz`;
 
 $ftpbot->ascii();
-open INDEX, "master.idx";
+open INDEX, "master";
 
 foreach $filing (<INDEX>) {
 
-	chomp $filing;
-	@fields = split /\|/, $filing;
+    chomp $filing;
+    @fields = split /\|/, $filing;
+    $fname = substr $fields[4], rindex($fields[4], "/") + 1;
 
-	if($fields[2] eq "10-Q") {
+    if($fields[2] eq "10-Q" && not -e $fname) {
 
-		print "\nFetch $fields[4]";	
-		$ftpbot->get("/" . $fields[4]) or print "\n" . $ftpbot->message;
-	}
+	print "\nFetch $fields[4]";	
+	$ftpbot->get("/" . $fields[4]) or print "\n" . $ftpbot->message;
+    } 
+
+    
 
 }
