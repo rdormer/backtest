71ba6cd609057b57db7b02feddb2dda243cf310e
Robert Dormer <rdormer@gmail.com>
2009-12-09 04:57:37 +0000

    Forgot to add EPS values to the database schema.

--- TOOLS/create_db.sql
+++ TOOLS/create_db.sql
@@ -3,7 +3,7 @@ create database finance;
 create table dividends (date date, ticker varchar(6), divamt float);
 create table splits (date date, ticker varchar(6), int bef, int after);
 create table historical (date date, ticker varchar(6), open float, high float, low float, close float, volume int unsigned);
-create table fundamentals(date date, sec_file char(24), sec_name varchar(100), sec_industry varchar(100), sic_code integer, total_assets integer);
+create table fundamentals(date date, sec_file char(24), sec_name varchar(100), sec_industry varchar(100), sic_code integer, total_assets integer, eps_basic float, eps_diluted float);
 
 create unique index histindex on historical (date, ticker);
 create unique index divindex on dividends (date, ticker);
