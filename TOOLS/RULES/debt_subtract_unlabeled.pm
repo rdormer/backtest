c2d870bd23f898006d208b9db290aae43f5b862f
Robert Dormer <rdormer@gmail.com>
2010-01-16 16:28:51 +0000

    Somehow missed a rule.

---
+++ TOOLS/RULES/debt_subtract_unlabeled.pm
@@ -0,0 +1,33 @@
+package debt_subtract_unlabeled;
+use parse_rule;
+
+our @ISA = qw(parse_rule);
+
+sub run {
+
+    my $self = shift;
+    my $res_hash = $self->result_hash;
+    return if ! exists $res_hash->{total_equity};
+    my @tuples = @{$self->get_tuples};
+
+    if(! exists $res_hash->{total_liabilities}) {
+
+	my $i, @last;
+
+	for($i = $#tuples; $i >= 0; $i--) {
+
+	    @last = @{ $tuples[$i] };
+	    last if $#last >= 3;
+	}
+
+	if($#last >= 3) {
+
+	    my $value = $last[$self->selection_offset + 2];
+	    my $dif = $value - $res_hash->{total_equity};
+	    $res_hash->{total_liabilities} = $dif;
+	    return;
+	}
+    }
+}
+
+1;
