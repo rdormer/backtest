ce7feb072371ecb0a23f1f2eb3c8e14ca4c39cba
Robert Dormer <rdormer@gmail.com>
2010-03-26 00:10:53 +0000

    Some more db schema optimizations (at least for MyISAM tables).

--- TOOLS/create_db.sql
+++ TOOLS/create_db.sql
@@ -1,9 +1,11 @@
 create database finance;
 use finance;
 
-create table dividends (date date, ticker varchar(6), divamt float) row_format=fixed;
-create table splits (date date, ticker varchar(6), bef integer, after integer) row_format=fixed;
-create table historical (date date, ticker varchar(6), open float, high float, low float, close float, volume integer unsigned) row_format=fixed;
+create table dividends (date date not null, ticker varchar(5) not null, divamt float not null) row_format=fixed;
+create table splits (date date not null, ticker varchar(5) not null, bef integer not null, after integer not null) row_format=fixed;
+
+create table historical (date date not null, ticker varchar(5) not null, open float not null, high float not null, 
+low float not null, close float not null, volume integer unsigned not null) row_format=fixed;
 
 create table fundamentals(filed_date date, quarter_date date, sec_file char(24), sec_name varchar(100), 
 sec_industry varchar(100), sic_code integer, cik char(10), total_assets integer unsigned, 
@@ -13,8 +15,9 @@ avg_shares_basic integer unsigned, avg_shares_diluted integer unsigned, eps_basi
 eps_diluted float, shares_authorized integer unsigned, shares_issued integer unsigned, 
 shares_outstanding integer unsigned);
 
-create unique index histindex on historical (date, ticker);
-create unique index divindex on dividends (date, ticker);
+create unique index histindex on historical (ticker, date);
+create unique index divindex on dividends (ticker, date);
+create unique index splitindex on splits (ticker, date);
 create unique index qtr_index on fundamentals (quarter_date, sec_name);
 create unique index filed_index on fundamentals (filed_date, sec_name);
 
