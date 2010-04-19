6a3cd5c6a29f1afebf6d3c6ca9270aca7419830b
Robert Dormer <rdormer@gmail.com>
2010-04-19 18:03:20 +0000

    Need to change the components script, yahoo has made some unspecified change to their markup that broke it.  Convert everything to plain text now before parsing, should insulate against future changes.

--- TOOLS/components.pl
+++ TOOLS/components.pl
@@ -1,27 +1,53 @@
 #! /usr/bin/perl
+use HTML::TreeBuilder;
 
 $ival = 1;
 $urlend = "%5E" . $ARGV[0];
 $getstring = "http://finance.yahoo.com/q/cp?s=" . $urlend;
 $stocktable = `wget -q -O - \"$getstring\"`;
-$flag = 1;
+my $count;
 
 do {
 
-    if($stocktable =~ /($urlend)([A-Z,-]+)(&amp)/) {
-	@stocks = split(/,/, $2);
+    my $parser = HTML::TreeBuilder->new;
+    $parser->parse_content($stocktable);
+    $parser->elementify();
+    my $ptext = extract_text($parser);
+
+    if($ptext =~ /Symbol Name Last Trade Change Volume(.+)All \|  A  \|  B  \|  C  \|  D  \|/) {
+
+	$count = 0;
+	@stocks = split(/[0-9]+,[0-9]+/, $1);
+
 	foreach $i (@stocks) {
-	    print "$i\n" if length $i > 0;
+
+	    if($i =~ /([A-Z,-]+) .*/) {
+		print "\n$1";
+		$count++;
+	    }
 	}
     }
-    else {
-	$flag = 0;
-    }
-
-
+    
     $url = $getstring . "&c=$ival";
     $stocktable = `wget -q -O - \"$url\"`;
     $ival++;
 
-} while ($flag);
+} while ($count > 0);
 
+
+
+sub extract_text {
+
+    my $parse_tree = shift;
+    my $page_text;
+
+    foreach my $c ($parse_tree->content_list) {
+        if(!ref $c) {
+            $page_text .= " $c";
+        } else {
+            $page_text .= extract_text($c);
+        }
+    }
+
+    return $page_text;
+}
