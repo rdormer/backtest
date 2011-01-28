c586ec516ff6953349be51d38bd52465ed494f82
Robert Dormer <rdormer@gmail.com>
2011-01-28 01:13:06 +0000

    New spider for the advfn.com fundamentals data source, with appropriate changes to db schema and sql code.

--- TOOLS/create_db.sql
+++ TOOLS/create_db.sql
@@ -9,18 +9,19 @@ create table historical (date date not null, ticker varchar(5) not null, open fl
 low float not null, close float not null, volume integer unsigned not null) row_format=fixed;
 
 create table fundamentals(filed_date date, quarter_date date, sec_file char(24), sec_name varchar(100), 
-sec_industry varchar(100), sic_code integer, cik char(10), total_assets integer unsigned, 
-current_assets integer unsigned, total_debt integer unsigned, current_debt integer unsigned, 
-cash integer unsigned, equity integer, net_income integer, revenue integer unsigned, 
-avg_shares_basic integer unsigned, avg_shares_diluted integer unsigned, eps_basic float, 
-eps_diluted float, shares_authorized integer unsigned, shares_issued integer unsigned, 
-shares_outstanding integer unsigned);
+sec_industry varchar(100), sic_code integer, cik char(10), total_assets bigint unsigned, 
+current_assets bigint unsigned, total_debt bigint unsigned, current_debt bigint unsigned, 
+cash bigint unsigned, equity integer, net_income integer, revenue bigint unsigned, 
+avg_shares_basic bigint unsigned, avg_shares_diluted bigint unsigned, eps_basic float, 
+eps_diluted float, shares_authorized bigint unsigned, shares_issued bigint unsigned, 
+shares_outstanding bigint unsigned, ticker varchar(5));
 
 create unique index histindex on historical (ticker, date);
 create unique index divindex on dividends (ticker, date);
 create unique index splitindex on splits (ticker, date);
-create unique index qtr_index on fundamentals (quarter_date, sec_name);
-create unique index filed_index on fundamentals (filed_date, sec_name);
+create unique index qtr_index_t on fundamentals (quarter_date, ticker);
+--create unique index qtr_index on fundamentals (quarter_date, sec_name);
+--create unique index filed_index on fundamentals (filed_date, sec_name);
 create unique index cik_index on fundamentals (cik, filed_date);
 create unique index cik_index on cikmap (ticker, cik);
 
