c30a8bfb37bd70ed8d545e72f987d6db8c01536d
Robert Dormer <rdormer@gmail.com>
2014-06-25 10:17:57 -0400

    Nasdaq site's ticker list broke, switch back to Yahoo

--- TOOLS/components.pl
+++ TOOLS/components.pl
@@ -1,18 +1,53 @@
 #! /usr/bin/perl
+use HTML::TreeBuilder;
 
-fetch_list($ARGV[0]);
+$ival = 1;
+$urlend = "%5E" . $ARGV[0];
+$getstring = "http://finance.yahoo.com/q/cp?s=" . $urlend;
+$stocktable = `wget -q -O - \"$getstring\"`;
+my $count;
 
-sub fetch_list {
+do {
 
-    my $exchange = shift;
-    my $url = "http://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=$exchange&render=download";
+    my $parser = HTML::TreeBuilder->new;
+    $parser->parse_content($stocktable);
+    $parser->elementify();
+    my $ptext = extract_text($parser);
 
-    my $raw = `wget -q "$url" -O -`;
-    my @lines = split /\n/, $raw;
+    if($ptext =~ /Symbol Name Last Trade Change Volume(.+)All \|  A  \|  B  \|  C  \|  D  \|/) {
 
-    foreach (@lines) {
-	my @parts = split /,/, $_;
-	$parts[0] =~ s/\"//g;
-	print "\n$parts[0]";
+	$count = 0;
+	@stocks = split(/%/, $1);
+
+	foreach $i (@stocks) {
+
+	    if($i =~ /([A-Z,-]+) .*/) {
+		print "\n$1";
+		$count++;
+	    }
+	}
     }
+    
+    $url = $getstring . "&c=$ival";
+    $stocktable = `wget -q -O - \"$url\"`;
+    $ival++;
+
+} while ($count > 0);
+
+
+
+sub extract_text {
+
+    my $parse_tree = shift;
+    my $page_text;
+
+    foreach my $c ($parse_tree->content_list) {
+        if(!ref $c) {
+            $page_text .= " $c";
+        } else {
+            $page_text .= extract_text($c);
+        }
+    }
+
+    return $page_text;
 }
